8a3cc4da8b54f3b55140fc619c7244fd6b01a810
Wojtek Kruk <krukon@gmail.com>
2014-01-21 17:31:12 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bandup::Application.config.secret_key_base = '21f7d559e594ac18d906646f068407cbf6bda6aa2bee45e138fa8c3bc84a72b876e55860ccf342dd0e2d293cb80c75f53924be8e11bedf3b3a4fc879dd080e41'
