82173b7ec829628571e89d7eac0c86d108136c81
Wojtek Kruk <krukon@gmail.com>
2014-01-24 15:52:49 +0100

    Changed the name of the migration

--- db/migrate/20140124144838_add_index_artist_instrument.rb
+++ db/migrate/20140124144838_add_index_artist_instrument.rb
@@ -1,4 +1,4 @@
-class AddIndexForUniquenessInArtistInstrumentRelations < ActiveRecord::Migration
+class AddIndexArtistInstrument < ActiveRecord::Migration
   def change
   	add_index :artist_instrument_relations, [:artist_id, :instrument_id], unique: true
   end
